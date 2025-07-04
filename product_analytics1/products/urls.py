2bf055b9ffcd42f10528da641ec48181241231bb
Abdulll02 <zeltyjvelosiped@gmail.com>
2025-07-04 17:29:04 +0300

    Теперь парсить можно прямо с сайта.

--- product_analytics1/products/urls.py
+++ product_analytics1/products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import ProductList
+from .views import ProductList, ParseView, ClearProductsView
 
 urlpatterns = [
-    path('api/products/', ProductList.as_view(), name='product-list')
+    path('api/products/', ProductList.as_view(), name='product-list'),
+    path('api/parse/', ParseView.as_view(), name='parse'),
+    path('api/clear_products/', ClearProductsView.as_view(), name='clear-products'),
 ]
--- product_analytics1/products/views.py
+++ product_analytics1/products/views.py
@@ -1,7 +1,10 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
 from django_filters import rest_framework as filters
+from .parser import parse_products
 
 class ProductFilter(filters.FilterSet):
     min_price = filters.NumberFilter(field_name='price', lookup_expr='gte')
@@ -18,3 +21,19 @@ class ProductList(generics.ListAPIView):
     serializer_class = ProductSerializer
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = ProductFilter
+
+class ParseView(APIView):
+    def post(self, request):
+        query = request.data.get('query')
+        if not query:
+            return Response({'error': 'Не передан параметр запроса'}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            parse_products(query)
+            return Response({'status': 'Парсинг завершён'}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class ClearProductsView(APIView):
+    def post(self, request):
+        Product.objects.all().delete()
+        return Response({'status': 'Таблица очищена'}, status=status.HTTP_200_OK)
